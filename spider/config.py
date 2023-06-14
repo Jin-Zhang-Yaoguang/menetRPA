215bd1c47a2e38a3b06ea90fc0cd1558940c9322
jin <18801041877>
2023-06-14 20:13:54 +0800

    Initial commit

---
+++ spider/config.py
@@ -0,0 +1,17 @@
+class spider_config:
+    CHROMEDRIVER_PATH = "/Users/jin/Downloads/chromedriver"
+    DATA_SAVE_PATH = "/Users/jin/Desktop/project/python/menetRPA/spider/data"
+    LOGIN_URL = "https://db.menet.com.cn/#/login"
+    DRUG_EXPLAIN = "https://db.menet.com.cn/#/drugexplain?nav=6&sort=5"
+    ACCOUNT = {
+        "account": "taiji",
+        "password": "123456"
+    }
+    RETRY_TIMES = 16
+
+
+
+
+
+
+
