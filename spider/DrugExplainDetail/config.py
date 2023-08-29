1aad89b82d680164e1452521849cac038d9c4c84
Jin-Zhang-Yaoguang <67220802+Jin-Zhang-Yaoguang@users.noreply.github.com>
2023-08-29 20:19:43 +0800

    Create config.py

---
+++ spider/DrugExplainDetail/config.py
@@ -0,0 +1,10 @@
+class spider_config:
+    CHROMEDRIVER_PATH = "/Users/jin/Downloads/chromedriver"
+    DATA_SAVE_PATH = "/Users/jin/Desktop/project/python/menetRPA/spider/spider/DrugExplainDetail/data/"
+    LOGIN_URL = "https://db.menet.com.cn/#/login"
+    DRUG_EXPLAIN_DETAIL = "https://db.menet.com.cn/#/DrugExplainDetail"
+    ACCOUNT = {
+        "account": "x",
+        "password": "x"
+    }
+    RETRY_TIMES = 16
