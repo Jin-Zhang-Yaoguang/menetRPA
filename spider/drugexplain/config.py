01976786321631b2e5a5fa5bf487e7f4fcb3d506
Jin-Zhang-Yaoguang <67220802+Jin-Zhang-Yaoguang@users.noreply.github.com>
2023-08-29 20:18:12 +0800

    Create config.py

---
+++ spider/drugexplain/config.py
@@ -0,0 +1,17 @@
+class spider_config:
+    CHROMEDRIVER_PATH = "/Users/jin/Downloads/chromedriver"
+    DATA_SAVE_PATH = "/spider/data"
+    LOGIN_URL = "https://db.menet.com.cn/#/login"
+    DRUG_EXPLAIN = "https://db.menet.com.cn/#/drugexplain?nav=6&sort=5"
+    ACCOUNT = {
+        "account": "x",
+        "password": "x"
+    }
+    RETRY_TIMES = 16
+
+
+
+
+
+
+
